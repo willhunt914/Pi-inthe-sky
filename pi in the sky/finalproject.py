cd72e37def246880350c4b7b9a3861604f4f28f3
jhelmke45 <113116262+jhelmke45@users.noreply.github.com>
2024-05-24 12:11:26 -0400

    Update finalproject.py

--- pi in the sky/finalproject.py
+++ pi in the sky/finalproject.py
@@ -1,4 +1,4 @@
-
+# Original data code from Afton
 import board
 import busio
 import adafruit_mpu6050
@@ -15,7 +15,7 @@ scl_pin = board.GP17
 i2c = busio.I2C(scl_pin, sda_pin)
 imu = adafruit_mpu6050.MPU6050(i2c,address = 0x68)
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address= 0x60)
-start = sensor.altitude 
+start = sensor.altitude # set initial height for altimeter to current height
 
 
 delay = .15
@@ -29,8 +29,8 @@ with open("/data.csv", "a") as datalog: # Opens / creates a file called data.csv
         else:
             led.value = False
             tilt = 0
-        diff = sensor.altitude - start
-        datalog.write(f"{monotonic()},{imu.acceleration[0]},{imu.acceleration[1]},{imu.acceleration[2]},{tilt},{diff}\n") # Writes the time, x, y, z acceleration, and if tilted to a file 
+        diff = sensor.altitude - start # height compared to starting height
+        datalog.write(f"{monotonic()},{imu.acceleration[0]},{imu.acceleration[1]},{imu.acceleration[2]},{tilt},{diff}\n") # Writes the time, x, y, z acceleration, tilt, and height difference to file
         datalog.flush()
 
         sled.value = True # Flashes onboard LED
